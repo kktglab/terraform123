e434595bcd3af47469b1942238595fdb021dac69
Kris Kas <krzysztof.kasprzyczak@gmail.com>
2025-08-27 06:20:21 +0200

    dw

--- 2nd-code.tf
+++ 2nd-code.tf
@@ -15,24 +15,35 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = "761d54cc-a644-4036-a5d4-7db14f4e62d6"
-}
+# provider "azurerm" {
+#   features {}
+#   subscription_id = "761d54cc-a644-4036-a5d4-7db14f4e62d6"
+# }
 
-resource "azurerm_resource_group" "milestonerg" {
-  name     = "rg-milestone-dev-01"
-  location = "westeurope"
-}
+# resource "azurerm_resource_group" "milestonerg" {
+#   name     = "rg-milestone-dev-01"
+#   location = "westeurope"
+# }
 
-resource "azurerm_storage_account" "samilestone" {
-  name                     = "samilestone001"
-  resource_group_name      = azurerm_resource_group.milestonerg.name
-  location                 = azurerm_resource_group.milestonerg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  tags = {
-    environment = "dev"
-    createdBy   = "Krzychu"
-  }
-}
+# resource "azurerm_storage_account" "samilestone" {
+#   name                     = "samilestone001"
+#   resource_group_name      = azurerm_resource_group.milestonerg.name
+#   location                 = azurerm_resource_group.milestonerg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+#   tags = {
+#     environment = "dev"
+#     createdBy   = "Krzychu"
+#   }
+# }
+
+# resource "azurerm_virtual_machine" "vm01" {
+#   name = "vmeusmall01"
+#   location = azurerm_resource_group.milestonerg.location
+#   resource_group_name = azurerm_resource_group.milestonerg.name
+#   storage_os_disk {
+#     disk_size_gb = 100
+#     create_option = 
+#   }
+#   vm_size = "Standard_B1s"
+# }
