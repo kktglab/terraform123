01aa91bb26f3bb7a076450525f9accafdcae2a88
Kris Kas <krzysztof.kasprzyczak@gmail.com>
2025-08-27 07:03:48 +0200

    abc;

--- 2nd-code.tf
+++ 2nd-code.tf
@@ -1,6 +1,6 @@
 # TERRAFORM CONFIGURATION BLOK
 terraform {
-  required_version = ">= 1.10"
+  required_version = ">= 1.13"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
